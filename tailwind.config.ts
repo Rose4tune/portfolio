b2b927edee9d88e860caf935461dfc51af201bec
Lee Ye Seo <rosefor2ne@gmail.com>
2025-06-11 09:42:49 +0900

    fix: PostCSS 및 Tailwind 설정 파일 정리

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,8 @@
 import type { Config } from "tailwindcss";
-import typography from "@tailwindcss/typography";
 
 const config: Config = {
   darkMode: "class",
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/content/**/*.{js,ts,jsx,tsx,mdx}",
@@ -66,7 +64,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [typography],
 };
 
 export default config;
