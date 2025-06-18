103b54215172938ae8a8a4c7cb63338385fae1f1
Lee Ye Seo <rosefor2ne@gmail.com>
2025-06-18 13:56:38 +0900

    chore: BlogPage에 Suspense 적용하여 로딩 상태 처리

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,14 @@
 import { getBlogPosts, getUniqueTags } from "@/lib/notion";
 import BlogPageClient from "./BlogPageClient";
+import { Suspense } from "react";
 
 export default async function BlogPage() {
   const posts = await getBlogPosts();
   const uniqueTags = await getUniqueTags();
 
-  return <BlogPageClient posts={posts} uniqueTags={uniqueTags} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <BlogPageClient posts={posts} uniqueTags={uniqueTags} />
+    </Suspense>
+  );
 }
